36a20638b02e6d3ba78f1c232c8890298c6f9e55
himanshu064 <himanshu064@gmail.com>
2021-12-11 11:33:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <div class="container px-5 py-24 mx-auto">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
